31b49f6706ec6ca1dc35f049f3311a2c13e055a1
LorenzCastillo <lorenz5524@hotmail.com>
2023-06-05 23:31:13 -0700

    Solved

---
+++ problems/6kyu - Create Phone Number/solution.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <iostream>
+
+std::string createPhoneNumber(const int arr [10]){
+  std::string phoneNumber = "(";
+  
+  for (int i = 0; i < 10; i++) {
+    if (i == 3) {
+      phoneNumber += ") ";
+    } 
+    if (i == 6) {
+      phoneNumber += "-";
+    }
+    phoneNumber += std::to_string(arr[i]);
+  }
+  
+  return phoneNumber;
+}
+
+int main() {
+  int testArr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+  std::cout << createPhoneNumber(testArr);
+
+  return 0;
+}
